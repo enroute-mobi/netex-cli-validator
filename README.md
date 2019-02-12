188b57c766dde81709a62389949a095ef74f48b6
Alban Peignier <alban@localhost.localdomain>
2019-02-12 14:04:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Install
+
+```
+bundle install
+```
+
+# Validate a file
+
+```
+bundle exec ruby netex-cli-validator.rb /path/to/file.xml
+```
---
+++ netex-cli-validator.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'nokogiri'
+
+def validate(document_path, schema_path, root_element)
+  schema = Nokogiri::XML::Schema(File.read(schema_path))
+  # document = Nokogiri::XML(File.read(document_path))
+  # schema.validate(document.xpath("//#{root_element}").to_s)
+  schema.validate(document_path)
+end
+
+target = File.expand_path(ARGV.first)
+
+Dir.chdir 'NeTEx/xsd' do
+  validate(target, 'NeTEx_publication.xsd', 'PublicationDelivery').each do |error|
+    puts error.message
+  end
+end
